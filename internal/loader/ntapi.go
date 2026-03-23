85d31ec4a24d6f8ad8c71a571358c3e8b8e4a157
Limkon <130622161+Limkon@users.noreply.github.com>
2026-03-23 14:34:05 +0800

    Create ntapi.go

---
+++ internal/loader/ntapi.go
@@ -0,0 +1,224 @@
+// 文件路径: internal/loader/ntapi.go
+package loader
+
+import (
+	"syscall"
+)
+
+var (
+	kernel32                     = syscall.NewLazyDLL("kernel32.dll")
+	ntdll                        = syscall.NewLazyDLL("ntdll.dll")
+	procCreateProcessW           = kernel32.NewProc("CreateProcessW")
+	procVirtualAllocEx           = kernel32.NewProc("VirtualAllocEx")
+	procWriteProcessMemory       = kernel32.NewProc("WriteProcessMemory")
+	procReadProcessMemory        = kernel32.NewProc("ReadProcessMemory")
+	procGetThreadContext         = kernel32.NewProc("GetThreadContext")
+	procSetThreadContext         = kernel32.NewProc("SetThreadContext")
+	procResumeThread             = kernel32.NewProc("ResumeThread")
+	procNtQueryInformationProcess = ntdll.NewProc("NtQueryInformationProcess")
+	procNtUnmapViewOfSection     = ntdll.NewProc("NtUnmapViewOfSection")
+)
+
+// --- 进程与线程结构体 ---
+type PROCESS_INFORMATION struct {
+	Process   syscall.Handle
+	Thread    syscall.Handle
+	ProcessId uint32
+	ThreadId  uint32
+}
+
+type STARTUPINFO struct {
+	Cb              uint32
+	LpReserved      *uint16
+	LpDesktop       *uint16
+	LpTitle         *uint16
+	DwX             uint32
+	DwY             uint32
+	DwXSize         uint32
+	DwYSize         uint32
+	DwXCountChars   uint32
+	DwYCountChars   uint32
+	DwFillAttribute uint32
+	DwFlags         uint32
+	WShowWindow     uint16
+	CbReserved2     uint16
+	LpReserved2     *byte
+	HStdInput       syscall.Handle
+	HStdOutput      syscall.Handle
+	HStdError       syscall.Handle
+}
+
+type PROCESS_BASIC_INFORMATION struct {
+	Reserved1       uintptr
+	PebBaseAddress  uintptr // PEB 基址
+	Reserved2       [2]uintptr
+	UniqueProcessId uintptr
+	Reserved3       uintptr
+}
+
+// --- 64位上下文结构体 (CONTEXT) ---
+// 必须严格按 16 字节对齐
+type M128A struct {
+	Low  uint64
+	High int64
+}
+
+type XMM_SAVE_AREA32 struct {
+	ControlWord    uint16
+	StatusWord     uint16
+	TagWord        byte
+	Reserved1      byte
+	ErrorOpcode    uint16
+	ErrorOffset    uint32
+	ErrorSelector  uint16
+	Reserved2      uint16
+	DataOffset     uint32
+	DataSelector   uint16
+	Reserved3      uint16
+	MxCsr          uint32
+	MxCsr_Mask     uint32
+	FloatRegisters [8]M128A
+	XmmRegisters   [16]M128A
+	Reserved4      [96]byte
+}
+
+type CONTEXT64 struct {
+	P1Home               uint64
+	P2Home               uint64
+	P3Home               uint64
+	P4Home               uint64
+	P5Home               uint64
+	P6Home               uint64
+	ContextFlags         uint32
+	MxCsr                uint32
+	SegCs                uint16
+	SegDs                uint16
+	SegEs                uint16
+	SegFs                uint16
+	SegGs                uint16
+	SegSs                uint16
+	EFlags               uint32
+	Dr0                  uint64
+	Dr1                  uint64
+	Dr2                  uint64
+	Dr3                  uint64
+	Dr6                  uint64
+	Dr7                  uint64
+	Rax                  uint64
+	Rcx                  uint64 // 存放 EntryPoint
+	Rdx                  uint64 // 存放 ImageBase
+	Rbx                  uint64
+	Rsp                  uint64
+	Rbp                  uint64
+	Rsi                  uint64
+	Rdi                  uint64
+	R8                   uint64
+	R9                   uint64
+	R10                  uint64
+	R11                  uint64
+	R12                  uint64
+	R13                  uint64
+	R14                  uint64
+	R15                  uint64
+	Rip                  uint64
+	FltSave              XMM_SAVE_AREA32
+	VectorRegister       [26]M128A
+	VectorControl        uint64
+	DebugControl         uint64
+	LastBranchToRip      uint64
+	LastBranchFromRip    uint64
+	LastExceptionToRip   uint64
+	LastExceptionFromRip uint64
+}
+
+const CONTEXT_FULL = 0x10000B
+
+// --- PE 结构体 ---
+type IMAGE_DOS_HEADER struct {
+	E_magic    uint16
+	E_cblp     uint16
+	E_cp       uint16
+	E_crlc     uint16
+	E_cparhdr  uint16
+	E_minalloc uint16
+	E_maxalloc uint16
+	E_ss       uint16
+	E_sp       uint16
+	E_csum     uint16
+	E_ip       uint16
+	E_cs       uint16
+	E_lfarlc   uint16
+	E_ovno     uint16
+	E_res      [4]uint16
+	E_oemid    uint16
+	E_oeminfo  uint16
+	E_res2     [10]uint16
+	E_lfanew   int32 // 指向 NT Header 的偏移
+}
+
+type IMAGE_FILE_HEADER struct {
+	Machine              uint16
+	NumberOfSections     uint16
+	TimeDateStamp        uint32
+	PointerToSymbolTable uint32
+	NumberOfSymbols      uint32
+	SizeOfOptionalHeader uint16
+	Characteristics      uint16
+}
+
+type IMAGE_OPTIONAL_HEADER64 struct {
+	Magic                       uint16
+	MajorLinkerVersion          uint8
+	MinorLinkerVersion          uint8
+	SizeOfCode                  uint32
+	SizeOfInitializedData       uint32
+	SizeOfUninitializedData     uint32
+	AddressOfEntryPoint         uint32
+	BaseOfCode                  uint32
+	ImageBase                   uint64
+	SectionAlignment            uint32
+	FileAlignment               uint32
+	MajorOperatingSystemVersion uint16
+	MinorOperatingSystemVersion uint16
+	MajorImageVersion           uint16
+	MinorImageVersion           uint16
+	MajorSubsystemVersion       uint16
+	MinorSubsystemVersion       uint16
+	Win32VersionValue           uint32
+	SizeOfImage                 uint32
+	SizeOfHeaders               uint32
+	CheckSum                    uint32
+	Subsystem                   uint16
+	DllCharacteristics          uint16
+	SizeOfStackReserve          uint64
+	SizeOfStackCommit           uint64
+	SizeOfHeapReserve           uint64
+	SizeOfHeapCommit            uint64
+	LoaderFlags                 uint32
+	NumberOfRvaAndSizes         uint32
+	DataDirectory               [16]IMAGE_DATA_DIRECTORY
+}
+
+type IMAGE_DATA_DIRECTORY struct {
+	VirtualAddress uint32
+	Size           uint32
+}
+
+type IMAGE_NT_HEADERS64 struct {
+	Signature      uint32
+	FileHeader     IMAGE_FILE_HEADER
+	OptionalHeader IMAGE_OPTIONAL_HEADER64
+}
+
+type IMAGE_SECTION_HEADER struct {
+	Name                 [8]byte
+	VirtualSize          uint32
+	VirtualAddress       uint32
+	SizeOfRawData        uint32
+	PointerToRawData     uint32
+	PointerToRelocations uint32
+	PointerToLinenumbers uint32
+	NumberOfRelocations  uint16
+	NumberOfLinenumbers  uint16
+	Characteristics      uint32
+}
